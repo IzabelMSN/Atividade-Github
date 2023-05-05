2ab92fae7f63a1d0e422ec37a134df7325d5a97a
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 19:18:31 -0300

    Logica de senha feita

--- 7/login.php
+++ 7/login.php
@@ -9,3 +9,9 @@ $email = readline("Insira seu e-mail:"."\n");
 $senha = readline("Insira sua senha: "."\n");
 
 $confisenha = readline("Confirme sua senha: "."\n");
+
+while ($senha != $confisenha) {
+    echo "Senhas diferentes. "."\n";
+    $senha = readline(" Digite a mesma senha: "."\n");
+    break;
+}
