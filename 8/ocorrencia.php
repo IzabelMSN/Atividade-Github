9e6be6562ae62960dea0c2486c3685a7db2a62fb
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 19:28:35 -0300

    Release: Logica corrigida

--- 8/ocorrencia.php
+++ 8/ocorrencia.php
@@ -3,8 +3,24 @@ $string = "Palavra";
 
 $palavra = strlen($string);
 
-for($i = 0; $i < $palavra; $i++){
-    $caractere = $string[$i];
+function resultado($string){
+    $string = str_replace(' ', ' ', $string);
 
-    echo "Número de ocorrência do caractere: $i"."Foi: $caractere\n";
+    $caractere = str_split($string);
+
+    $resultado = [];
+
+    foreach ($caractere as $caractere_k) {
+        if (!isset($resultado[$caractere_k])){
+            $resultado[$caractere_k] = 1;
+        }else {
+            $resultado[$caractere_k] += 1;
+        }
+    }
+
+    foreach($resultado as $caractere_k => $caractere_v){
+        echo "Número de ocorrência do caractere: ".$caractere_k."Foi: ".$caractere_v."\n";
+    }
 }
+
+resultado($string);
