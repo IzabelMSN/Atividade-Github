3e081111056631d359b1ccccfb9bce9336253f05
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 20:03:51 -0300

    Adicionando dados

---
+++ 10/search.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Ache pessoas que moram na mesma cidade que a sua."."\n";
+
+$usuarioCidade = readline("Informe a cidade em que você vive: ");
+
+$pessoas = array(
+    "pessoas1" => [
+        "nome" => "Leôncio",
+        "idade" => 43,
+        "cidade" => "São Paulo"
+    ],
+
+    "pessoas2" => [
+        "nome" => "Verônica",
+        "idade" => 27,
+        "cidade" => "Porto Alegre"
+    ],
+
+    "pessoas3" => [
+        "nome" => "Ashley",
+        "idade" => 27,
+        "cidade" => "Goiânia"
+    ],  
+);
+
+
+
+
