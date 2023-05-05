eb1a18236a50e47ae47c70c53685619121b8bd6e
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 18:37:17 -0300

    Terminando logica -A

--- 6/funcionabilidadeA.php
+++ 6/funcionabilidadeA.php
@@ -3,3 +3,10 @@
 echo "Funcionalidade 2";
 
 echo "Fazendo funcionalidade 2 -> Senha";
+
+$senha = readline("Digite sua senha: ");
+
+while ($senha != "1234") {
+    echo "Senha incorreta. ";
+    $senha = readline(" Digite a senha correta: ");
+}
