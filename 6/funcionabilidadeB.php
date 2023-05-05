8b7dcfa0ecc36e8f49a05c059adf35dc5852c7f8
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 18:39:23 -0300

    Terminando logica -B

--- 6/funcionabilidadeB.php
+++ 6/funcionabilidadeB.php
@@ -3,3 +3,10 @@
 echo "Funcionalidade B"."\n";
 
 echo "Fazendo funcionalidade B -> E-mail"."\n";
+
+$email = readline("Digite sua e-mail: "."\n");
+
+while ($email != "lolo@gmail.com") {
+    echo "E-mail incorreto. ";
+    $email = readline(" Digite o e-mail correto: ");
+}
