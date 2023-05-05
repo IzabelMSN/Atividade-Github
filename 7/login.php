5aece28bb222854d037150b6ba637cda2222e52f
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-05 19:17:38 -0300

    Começando desenvolvimento de senha

--- 7/login.php
+++ 7/login.php
@@ -5,3 +5,7 @@ echo "Insira suas infomações para login"."\n";
 $user = readline("Insira o nome do seu usuário: "."\n");
 
 $email = readline("Insira seu e-mail:"."\n");
+
+$senha = readline("Insira sua senha: "."\n");
+
+$confisenha = readline("Confirme sua senha: "."\n");
